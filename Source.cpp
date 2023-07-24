99d3a8ffd5eb8b6871beb08e5cc7f8e3ce55097e
muhammad saqlain arshad <saqlainarshad3266@gmail.com>
2023-07-24 12:28:35 +0500

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<numeric>
+using namespace std;
+int main()
+{
+	int n = 100, sum = 0,nsum=0,fn=0,dif=0;
+	for (int i = 1; i <= n; i++)
+	{
+		sum += (i * i);
+		nsum += i;
+	}
+	fn=(nsum * nsum);
+	dif = fn - sum;
+	cout<<dif;
+
+	return 0;
+}
